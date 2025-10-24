1e5785c05f656f1e00e9d7d3b24662f2a46f93f9
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-10-23 19:25:09 -0500

    Add comments

--- scripts/clone_vfv_repos.bash
+++ scripts/clone_vfv_repos.bash
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Downloads all VulFixVUl files into a directory.
+# Expects a JSON file of the VulFixVul repos as an array of objects with each
+# object have a "url" key whose value is a clone-able `git` repository as input
+
 VFV_JSON_FILE=$1
 
 jq -r '.[].url' "$VFV_JSON_FILE" | sort -u | while read -r url; do
--- scripts/download_cwe_list_4.17.bash
+++ scripts/download_cwe_list_4.17.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Downloads latest CWE infomration in XML format
+
 ZIP_FILENAME="../cwe/cwe.zip"
 
 wget -nc -O $ZIP_FILENAME https://cwe.mitre.org/data/xml/cwec_latest.xml.zip
